3e401447780b4bcc226aefd7c99212177c5072e4
githukudenis <githukudenis@gmail.com>
2023-10-24 05:30:06 +0300

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,19 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+buildscript {
+    dependencies {
+        classpath(libs.hilt.android.gradle.plugin)
+        classpath(libs.google.services)
+        classpath(libs.firebase.crashlytics.gradlePlugin)
+        classpath(libs.perf.plugin)
+    }
+}
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.gms) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
