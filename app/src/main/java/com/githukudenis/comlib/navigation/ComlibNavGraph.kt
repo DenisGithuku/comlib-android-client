07f4728fc436c86d539cdfb0aa2a7f189958b60c
githukudenis <githukudenis@gmail.com>
2023-12-06 01:02:11 +0300

    Add transition to profile and add book routes

--- app/src/main/java/com/githukudenis/comlib/navigation/ComlibNavGraph.kt
+++ app/src/main/java/com/githukudenis/comlib/navigation/ComlibNavGraph.kt
@@ -1,10 +1,15 @@
 package com.githukudenis.comlib.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.githukudenis.comlib.app.AppState
+import com.githukudenis.comlib.feature.add_book.AddBookRoute
 import com.githukudenis.comlib.feature.book_detail.BookDetailRoute
+import com.githukudenis.comlib.feature.my_books.MyBooksRoute
 import com.githukudenis.comlib.feature.profile.ProfileRoute
 import com.githukudenis.comlib.onboarding.OnBoardingScreen
 
@@ -47,10 +52,34 @@ fun ComlibNavGraph(
                 appState.popBackStack()
             })
         }
-        composable(route = ComlibDestination.Profile.route) {
+        composable(enterTransition = {
+            slideIntoContainer(towards = AnimatedContentTransitionScope.SlideDirection.Left) + fadeIn()
+        }, exitTransition = {
+            slideOutOfContainer(towards = AnimatedContentTransitionScope.SlideDirection.Right) + fadeOut()
+        }, route = ComlibDestination.Profile.route) {
             ProfileRoute(onBackPressed = {
                 appState.popBackStack()
+            }, onOpenMyBooks = {
+                appState.navigate(
+                    route = ComlibDestination.MyBooks.route,
+                    popUpTo = ComlibDestination.MyBooks.route
+                )
             })
+        }
+        composable(enterTransition = {
+            slideIntoContainer(towards = AnimatedContentTransitionScope.SlideDirection.Left) + fadeIn()
+        }, exitTransition = {
+            slideOutOfContainer(towards = AnimatedContentTransitionScope.SlideDirection.Right) + fadeOut()
+        }, route = ComlibDestination.MyBooks.route
+        ) {
+            MyBooksRoute(onNavigateToBookDetails = {}, onNavigateToAddBook = { /*TODO*/ }) {
+
+            }
+        }
+        composable(route = ComlibDestination.AddBook.route) {
+            AddBookRoute(onNavigateUp = {
+
+            }, onBookAdded = {})
         }
     }
 }
@@ -69,4 +98,6 @@ sealed class ComlibDestination(
 
     data object GetStarted : ComlibDestination(route = "get_started")
     data object Profile : ComlibDestination(route = "profile")
+    data object MyBooks : ComlibDestination(route = "my_books")
+    data object AddBook : ComlibDestination(route = "add_book")
 }
