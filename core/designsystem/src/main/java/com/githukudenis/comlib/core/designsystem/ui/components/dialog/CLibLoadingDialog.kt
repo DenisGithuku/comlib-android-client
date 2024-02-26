e2bbbd8476aa2ad4189fc4d67ab369cfd14c11cb
githukudenis <githukudenis@gmail.com>
2024-02-26 16:29:58 +0300

    Add custom loading dialog.

---
+++ core/designsystem/src/main/java/com/githukudenis/comlib/core/designsystem/ui/components/dialog/CLibLoadingDialog.kt
@@ -0,0 +1,57 @@
+package com.githukudenis.comlib.core.designsystem.ui.components.dialog
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.githukudenis.comlib.core.designsystem.R
+import com.githukudenis.comlib.core.designsystem.ui.components.loading_indicators.CLibCircularProgressBar
+
+@Composable
+fun CLibLoadingDialog(modifier: Modifier = Modifier, label: String? = null, onDismissRequest: () -> Unit) {
+    Dialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(0.9f)
+                .height(70.dp),
+            shape = MaterialTheme.shapes.large
+        ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = label ?: stringResource(id = R.string.default_loading_indicator),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onSurface.copy(
+                            alpha = 0.7f
+                        )
+                    )
+                    CLibCircularProgressBar()
+                }
+        }
+    }
+}
+
+@Preview(device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420")
+@Composable
+private fun DialogPreview() {
+    CLibLoadingDialog {
+
+    }
+}
